b26b2f3679a167661b171bd8d4f2eeffcf74ccb9
Pulido-Justine <103050199+Pulido-Justine@users.noreply.github.com>
2022-04-11 21:19:01 +0800

    design
    ScorePosition

--- tictacball.py
+++ tictacball.py
@@ -76,3 +76,30 @@ def evaluate_window(window, piece):
 		score -= 4
 		
 	return score	
+def score_position(board, piece):
+	score = o
+	
+	center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+	center_count = center_array.count(piece)
+	score += center_count * 3
+	
+	for r in range(ROW_COUNT):
+		row_array = [int(i) for i in list(board[r, :])]
+		for c in range(COLUMN_COUNT-3):
+			window = row_array[c:c+WINDOW_LENGTH]score += evaluate_window(window, piece)
+			
+	for c in range(COLUMN_COUNT):
+		col_array = [int(i) for i in list(board[:,c])]
+		for r in range(ROW_COUNT-3):
+			window = col_array[r:r+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+			
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [boardr+i][c+i]for i in range(WINDOW_LENGTH)] score += evaluate_window(window, piece)
+			
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+3+i][c+i] for i in range(WINDOW_LENGTH)] score += evaluate_window(window, piece)
+			
+		return score
