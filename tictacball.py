5fc3721cd573777c6d795a9fc1ad19d959769042
Catherine Medina <103050333+Medina-Catherine-Tenorio@users.noreply.github.com>
2022-04-29 00:19:04 +0800

    main_menu

--- tictacball.py
+++ tictacball.py
@@ -373,7 +373,16 @@ board = create_board()
 
             if game_over:
                 pygame.time.wait(3000)
-				
+		
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+	
+	MENU_MOUSE_POS = pygame.mouse.get_pos()
 
+        MENU_TEXT = get_font(120).render("TicTac Ball", True, "#ca6702")
+        MENU_RECT = MENU_TEXT.get_rect(center=(400, 150))
+
+        
 				
 			 
