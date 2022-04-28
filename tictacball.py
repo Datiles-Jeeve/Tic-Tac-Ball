e4c02206614ef4affc1d6df4b1772c7e71e2d173
Catherine Medina <103050333+Medina-Catherine-Tenorio@users.noreply.github.com>
2022-04-28 23:32:05 +0800

    draw board function

--- tictacball.py
+++ tictacball.py
@@ -237,8 +237,16 @@ def play():
                     best_score = score
                     best_col = col
 
-            return best_col
+            return best_col	
 
+	def draw_board(board):
+            for c in range(COLUMN_COUNT):
+                for r in range(ROW_COUNT):
+                    pygame.draw.rect(screen, gray,
+                                     (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))
+                    pygame.draw.circle(screen, BLACK, (
+                        int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)),
+                                       RADIUS)
 
 	
 
