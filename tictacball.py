fe7e10a7c10ee31ee0c2a71d187a4ed90f55beb0
jeeve <datiles.059955@balayan.sti.edu.ph>
2022-04-28 23:20:41 +0800

    play

--- tictacball.py
+++ tictacball.py
@@ -26,6 +26,7 @@ FPS = 60
 
 BG = pygame.image.load("assets/bg.png")
 
+
 def get_font(size):
     return pygame.font.Font("assets/burbank.otf", size)
 
@@ -56,14 +57,14 @@ def play():
         player.set_volume(0.2)
         mixer.music.stop()
 	
-	BLUE = (0, 0, 255)
+		BLUE = (0, 0, 255)
         BLACK = (0, 0, 0)
         RED = (255, 0, 0)
         YELLOW = (255, 255, 0)
         gray = (206, 212, 218)
         ORANGE = (202, 103, 2)
 	
-	ROW_COUNT = 6
+		ROW_COUNT = 6
         COLUMN_COUNT = 7
 
         PLAYER = 0
@@ -75,9 +76,9 @@ def play():
 
         WINDOW_LENGTH = 4
 	
-	def create_board():
-            board = np.zeros((ROW_COUNT, COLUMN_COUNT))
-            return board
+		def create_board():
+         	board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+         	return board
 
         def drop_piece(board, row, col, piece):
             board[row][col] = piece
@@ -89,35 +90,35 @@ def play():
             for r in range(ROW_COUNT):
                 if board[r][col] == 0:
                     return r
-	def print_board(board):
+		def print_board(board):
             print(np.flip(board, 0))
 
         def winning_move(board, piece):
-		for c in range(COLUMN_COUNT - 3):
+			for c in range(COLUMN_COUNT - 3):
                 for r in range(ROW_COUNT):
                     if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
                         c + 3] == piece:
                         return True
 		
-		for c in range(COLUMN_COUNT):
+			for c in range(COLUMN_COUNT):
                 for r in range(ROW_COUNT - 3):
                     if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
                         c] == piece:
                         return True
 		
-		for c in range(COLUMN_COUNT - 3):
+			for c in range(COLUMN_COUNT - 3):
                 for r in range(ROW_COUNT - 3):
                     if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and \
                             board[r + 3][c + 3] == piece:
                         return True
 		
-		for c in range(COLUMN_COUNT - 3):
+			for c in range(COLUMN_COUNT - 3):
                 for r in range(3, ROW_COUNT):
                     if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and \
                             board[r - 3][c + 3] == piece:
                         return True
 		
-	def evaluate_window(window, piece):
+		def evaluate_window(window, piece):
             score = 0
             opp_piece = PLAYER_PIECE
             if piece == PLAYER_PIECE:
@@ -135,17 +136,18 @@ def play():
 
             return score
 
-def score_position(board, piece):
+		def score_position(board, piece):
             score = 0
+
 		center_array = [int(i) for i in list(board[:, COLUMN_COUNT // 2])]
-            center_count = center_array.count(piece)
-            score += center_count * 3
+        center_count = center_array.count(piece)
+        score += center_count * 3
 		
 		for r in range(ROW_COUNT):
-                row_array = [int(i) for i in list(board[r, :])]
-                for c in range(COLUMN_COUNT - 3):
-                    window = row_array[c:c + WINDOW_LENGTH]
-                    score += evaluate_window(window, piece)
+            row_array = [int(i) for i in list(board[r, :])]
+            for c in range(COLUMN_COUNT - 3):
+                window = row_array[c:c + WINDOW_LENGTH]
+                score += evaluate_window(window, piece)
 			
 		for c in range(COLUMN_COUNT):
                 col_array = [int(i) for i in list(board[:, c])]
@@ -154,16 +156,52 @@ def score_position(board, piece):
                     score += evaluate_window(window, piece)
 			
 		for r in range(ROW_COUNT - 3):
-                for c in range(COLUMN_COUNT - 3):
-                    window = [board[r + i][c + i] for i in range(WINDOW_LENGTH)]
-                    score += evaluate_window(window, piece)
+            for c in range(COLUMN_COUNT - 3):
+                window = [board[r + i][c + i] for i in range(WINDOW_LENGTH)]
+                score += evaluate_window(window, piece)
 			
 		for r in range(ROW_COUNT - 3):
-                for c in range(COLUMN_COUNT - 3):
-                    window = [board[r + 3 - i][c + i] for i in range(WINDOW_LENGTH)]
-                    score += evaluate_window(window, piece)
+            for c in range(COLUMN_COUNT - 3):
+                window = [board[r + 3 - i][c + i] for i in range(WINDOW_LENGTH)]
+                score += evaluate_window(window, piece)
 
-            return score	
+        return score
+
+
+	def is_terminal_node(board):
+            return winning_move(board, PLAYER_PIECE) or winning_move(board, AI_PIECE) or len(
+                get_valid_locations(board)) == 0
+
+        def minimax(board, depth, alpha, beta, maximizingPlayer):
+            valid_locations = get_valid_locations(board)
+            is_terminal = is_terminal_node(board)
+            if depth == 0 or is_terminal:
+                if is_terminal:
+                    if winning_move(board, AI_PIECE):
+                        return (None, 100000000000000)
+                    elif winning_move(board, PLAYER_PIECE):
+                        return (None, -10000000000000)
+                    else:  # Game is over, no more valid moves
+                        return (None, 0)
+                else:  # Depth is zero
+                    return (None, score_position(board, AI_PIECE))
+            if maximizingPlayer:
+                value = -math.inf
+                column = random.choice(valid_locations)
+                for col in valid_locations:
+                    row = get_next_open_row(board, col)
+                    b_copy = board.copy()
+                    drop_piece(b_copy, row, col, AI_PIECE)
+                    new_score = minimax(b_copy, depth - 1, alpha, beta, False)[1]
+                    if new_score > value:
+                        value = new_score
+                        column = col
+                    alpha = max(alpha, value)
+                    if alpha >= beta:
+                        break
+                return column, value
+
+
 	
 
 
@@ -173,37 +211,7 @@ def score_position(board, piece):
 	
 	
 	
-def is_terminal_node(board):
-	return winning_move(board, PLAYER_PIECE) or winning_move(board, AI_PIECE) or len(get_valid_locations(board)) == 0
 
-def minimax(board, depth, alpha, beta, maximizingPlayer):
-	valid_locations = get_valid_locations(board)
-	is_terminal = is_terminal_node(board)
-	if depth == 0 or is_terminal:
-		if is_terminal:
-			if winning_move(board, AI_PIECE):
-				return (None, 100000000000000)
-			elif winning_move(board, PLAYER_PIECE):
-				return (None, -10000000000000)
-			else: # Game is over, no more valid moves
-				return (None, 0)
-		else: # Depth is zero
-			return (None, score_position(booard, AI_PIECE)
-	if maximizingPlayer:
-		value = -math.inf
-		column = random.choice(valid_locations)
-		for col in valid_locations:
-			row = get_next_open_row(board, col)
-			b_copy = board.copy()
-			drop_piece(b_copy, row, col, AI_PIECE)
-			new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
-			if new_score > value:
-				value = new_score
-				column = col
-			alpha = max(alpha, value)
-			if alpha >= beta:
-				break
-		return column, value
 	else:
 		value = math.inf
 		column = random.choice(valid_locations)
