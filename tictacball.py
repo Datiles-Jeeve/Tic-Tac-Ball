de2c263a9de8d7b60e4b99cf0012c711fbdb476c
Pulido-Justine <103050199+Pulido-Justine@users.noreply.github.com>
2022-04-28 23:47:05 +0800

    minimizing player

--- tictacball.py
+++ tictacball.py
@@ -285,6 +285,45 @@ board = create_board()
         myfont = pygame.font.SysFont("monospace", 75)
 
         turn = random.randint(PLAYER, AI)
+	
+	else:  
+                value = math.inf
+                column = random.choice(valid_locations)
+                for col in valid_locations:
+                    row = get_next_open_row(board, col)
+                    b_copy = board.copy()
+                    drop_piece(b_copy, row, col, PLAYER_PIECE)
+                    new_score = minimax(b_copy, depth - 1, alpha, beta, True)[1]
+                    if new_score < value:
+                        value = new_score
+                        column = col
+                    beta = min(beta, value)
+                    if alpha >= beta:
+                        break
+                return column, value
+
+        def get_valid_locations(board):
+            valid_locations = []
+            for col in range(COLUMN_COUNT):
+                if is_valid_location(board, col):
+                    valid_locations.append(col)
+            return valid_locations
+
+        def pick_best_move(board, piece):
+
+            valid_locations = get_valid_locations(board)
+            best_score = -10000
+            best_col = random.choice(valid_locations)
+            for col in valid_locations:
+                row = get_next_open_row(board, col)
+                temp_board = board.copy()
+                drop_piece(temp_board, row, col, piece)
+                score = score_position(temp_board, piece)
+                if score > best_score:
+                    best_score = score
+                    best_col = col
+
+            return best_col
     
 			
 
