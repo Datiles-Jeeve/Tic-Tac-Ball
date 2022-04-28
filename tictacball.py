8cbc6e7eb6934e69c731257d4940ffa01b45040b
Catherine Medina <103050333+Medina-Catherine-Tenorio@users.noreply.github.com>
2022-04-28 22:30:45 +0800

    update fonts

--- tictacball.py
+++ tictacball.py
@@ -25,24 +25,9 @@ clock = pygame.time.Clock()
 FPS = 60
 
 BG = pygame.image.load("assets/bg.png")
-###########################tictacball
 
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
-
-ROW_COUNT = 6
-COLUMN_COUNT = 7
-
-PLAYER = 0
-AI = 1
-
-EMPTY = 0
-PLAYER_PIECE = 1
-AI_PIECE = 2
-
-WINDOW_LENGTH = 4
+def get_font(size):
+    return pygame.font.Font("assets/burbank.otf", size)
 
 def create_board():
   board = np.zeros((ROW_COUNT,COLUMN_COUNT))
