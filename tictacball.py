b36f057c28ff2e8ced7d2f058fd7f8f3f2062baf
Maullon-Jenny <103049893+Maullon-Jenny@users.noreply.github.com>
2022-04-28 07:58:32 -0700

    Update tictacball.py

--- tictacball.py
+++ tictacball.py
@@ -138,39 +138,7 @@ def play():
 
 
     
-		
-for c in range (COLUMN_COUNT):
-	for r in range (ROW_COUNT-3):
-		if board [r][c] == piece and board [r+1][c] == piece and board [r+3][c] == piece:
-			return True
-		
-for c in range (COLUMN_COUNT-3):
-	for r in range (ROW_COUNT-3)
-		if board [r][c] == piece and board [r+1][c] == piece and board [r+2][c+2] == piece and board [r+3][c+3] == piece:
-			return True
-
-for c in range (COLUMN_COUNT-3):
-	for r in range (3, ROW_COUNT):
-		if board [r][c] == piece and board [r-1][c+1] == piece and board [r-3][c+3] == piece:
-			return True
-
-def evaluate_window(window, piece):
-	score = 0
-	opp_piece = PLAYER_PIECE
-	if piece == PLAYER_PIECE:
-		opp_piece = AI_PIECE
-		
-	if window.count(piece) == 4:
-		score += 100
-	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-		score += 5
-	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
-		score += 2
-		
-	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
-		score -= 4
-		
-	return score	
+			
 def score_position(board, piece):
 	score = o
 	
