e9de10e23c135929dc1ed428d8c6ed6b58b3fca7
Pulido-Justine <103050199+Pulido-Justine@users.noreply.github.com>
2022-04-28 23:56:00 +0800

    game over

--- tictacball.py
+++ tictacball.py
@@ -286,44 +286,19 @@ board = create_board()
 
         turn = random.randint(PLAYER, AI)
 	
-	else:  
-                value = math.inf
-                column = random.choice(valid_locations)
-                for col in valid_locations:
-                    row = get_next_open_row(board, col)
-                    b_copy = board.copy()
-                    drop_piece(b_copy, row, col, PLAYER_PIECE)
-                    new_score = minimax(b_copy, depth - 1, alpha, beta, True)[1]
-                    if new_score < value:
-                        value = new_score
-                        column = col
-                    beta = min(beta, value)
-                    if alpha >= beta:
-                        break
-                return column, value
+	while not game_over:
 
-        def get_valid_locations(board):
-            valid_locations = []
-            for col in range(COLUMN_COUNT):
-                if is_valid_location(board, col):
-                    valid_locations.append(col)
-            return valid_locations
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-        def pick_best_move(board, piece):
+                if event.type == pygame.MOUSEMOTION:
+                    pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+                    posx = event.pos[0]
+                    if turn == PLAYER:
+                        pygame.draw.circle(screen, BLUE, (posx, int(SQUARESIZE / 2)), RADIUS)
 
-            valid_locations = get_valid_locations(board)
-            best_score = -10000
-            best_col = random.choice(valid_locations)
-            for col in valid_locations:
-                row = get_next_open_row(board, col)
-                temp_board = board.copy()
-                drop_piece(temp_board, row, col, piece)
-                score = score_position(temp_board, piece)
-                if score > best_score:
-                    best_score = score
-                    best_col = col
-
-            return best_col
+                pygame.display.update()
     
 			
 
@@ -336,19 +311,7 @@ board = create_board()
 	
 			
 
-while not game_over:
-	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-				
-	if event.type == pygame.MOUSEMOTION:
-		pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-		posx = event.pos[0]
-		if turn == PLAYER:
-			pygame.draw.circle (screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
-				
-	pygame.display.update()
+
 				
 	if event.type == pygame.MOUSEBUTTONDOWN:
 		pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
